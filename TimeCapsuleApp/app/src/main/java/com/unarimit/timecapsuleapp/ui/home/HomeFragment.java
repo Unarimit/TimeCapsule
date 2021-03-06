7daabc25cc2b47ecb9c7bb2b2a05cafc051da1c1
Unarimit <1798907875@qq.com>
2021-03-06 23:30:38 +0800

    completed three lines display framework in home page

--- TimeCapsuleApp/app/src/main/java/com/unarimit/timecapsuleapp/ui/home/HomeFragment.java
+++ TimeCapsuleApp/app/src/main/java/com/unarimit/timecapsuleapp/ui/home/HomeFragment.java
@@ -9,8 +9,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
-import android.os.SystemClock;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,23 +16,19 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.unarimit.timecapsuleapp.MainActivity;
 import com.unarimit.timecapsuleapp.R;
 import com.unarimit.timecapsuleapp.entities.Period;
-import com.unarimit.timecapsuleapp.ui.common.IconStringList;
+import com.unarimit.timecapsuleapp.entities.Task;
 import com.unarimit.timecapsuleapp.ui.common.IconTextView;
-import com.unarimit.timecapsuleapp.ui.task.TaskFragment;
 import com.unarimit.timecapsuleapp.utils.database.DbContext;
 
-import java.util.Locale;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HomeFragment extends Fragment {
 
@@ -64,18 +58,45 @@ public class HomeFragment extends Fragment {
         activity.refreshFragment(this);
     }
 
-    RecyclerView recyclerView;
+    RecyclerView oftenTaskRecyclerView;
+    RecyclerView unOftenTaskRecyclerView;
+    TextView noTaskHint;
     private void NoCurrentPeriodCreate(){
         // init task grid
         // create period in recyclerAdapter
-        recyclerView = root.findViewById(R.id.home_task_grid);
+        oftenTaskRecyclerView = root.findViewById(R.id.home_task_often_grid);
+        unOftenTaskRecyclerView = root.findViewById(R.id.home_task_unoften_grid);
+        noTaskHint = root.findViewById(R.id.home_task_notask);
         if(homeViewModel.getTasks() == null || homeViewModel.getTasks().isEmpty()){
-            recyclerView.setVisibility(View.GONE);
+            oftenTaskRecyclerView.setVisibility(View.GONE);
+            unOftenTaskRecyclerView.setVisibility(View.GONE);
+            noTaskHint.setVisibility(View.VISIBLE);
         }else{
-            recyclerView.setLayoutManager(new GridLayoutManager(root.getContext(), 4));
-            recyclerView.setAdapter(new HomeTaskViewAdapter(homeViewModel.getTasks(), this));
+            noTaskHint.setVisibility(View.GONE);
+            List<Task> oftenTask = homeViewModel.getTasks().stream().filter(x -> x.isOften()).collect(Collectors.toList());
+            List<Task> unOftenTask = homeViewModel.getTasks().stream().filter(x -> !x.isOften()).collect(Collectors.toList());
+            if(oftenTask.isEmpty()){
+                root.findViewById(R.id.home_task_often_layout).setVisibility(View.GONE);
+            }else{
+                root.findViewById(R.id.home_task_often_layout).setVisibility(View.VISIBLE);
+                oftenTaskRecyclerView.setLayoutManager(new GridLayoutManager(root.getContext(), 4));
+                oftenTaskRecyclerView.setAdapter(new HomeTaskViewAdapter(oftenTask, this));
+            }
+
+            root.findViewById(R.id.home_task_curve_layout).setVisibility(View.GONE);
+
+            if(unOftenTask.isEmpty()){
+                root.findViewById(R.id.home_task_unoften_layout).setVisibility(View.GONE);
+            }else{
+                root.findViewById(R.id.home_task_unoften_layout).setVisibility(View.VISIBLE);
+                unOftenTaskRecyclerView.setLayoutManager(new GridLayoutManager(root.getContext(), 4));
+                unOftenTaskRecyclerView.setAdapter(new HomeTaskViewAdapter(unOftenTask, this));
+            }
+
         }
     }
+
+    // following is on task config
     IconTextView icon;
     TextView timeText;
     TextView taskDescText;
