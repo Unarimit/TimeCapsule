afbad0f02e8e2ba92ecafd0b8d089becd6974731
Unarimit <1798907875@qq.com>
2021-03-12 16:46:13 +0800

    add TimeHelper and test

---
+++ TimeCapsule/Infrastructure/Utils/TimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeCapsule.Infrastructure.Utils
+{
+    public class TimeHelper
+    {
+        public static DateTime ConvertFromUnixTimestamp(long timestamp)
+        {
+            return new DateTime(1970, 1, 1, 0, 0, 0)
+                .AddSeconds(timestamp);
+        }
+
+        public static long ConvertToUnixTimestamp(DateTime dateTime)
+        {
+            DateTime startTime = new DateTime(1970, 1, 1, 0, 0, 0);
+            return (int)(dateTime - startTime).TotalSeconds;
+        }
+    }
+}
---
+++ TimeCapsule/tests/InfrastructureTests/Utils/TimeHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeCapsule.Infrastructure.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeCapsule.Infrastructure.Utils.Tests
+{
+    [TestClass()]
+    public class TimeHelperTests
+    {
+        [TestMethod()]
+        public void ConvertFromUnixTimestampTest()
+        {
+            DateTime date = TimeHelper.ConvertFromUnixTimestamp(1615507200);
+            DateTime target = new DateTime(2021, 3, 12, 0, 0, 0);
+            Assert.AreEqual(date, target);
+        }
+
+        [TestMethod()]
+        public void ConvertToUnixTimestampTest()
+        {
+            long date = TimeHelper.ConvertToUnixTimestamp(new DateTime(2021, 3, 12, 0, 0, 0));
+            long target = 1615507200;
+            Assert.AreEqual(date, target);
+        }
+    }
+}
